d7c18473b4bac4db458092d8c52ee27bc3545f67
Aleksey V Zapparov <ixti@member.fsf.org>
2012-07-18 11:51:55 +0200

    Fix Makefile. Add test stub

---
+++ test/test_puncher.js
@@ -0,0 +1,26 @@
+/*global describe, it, before, after, beforeEach, afterEach*/
+
+
+'use strict';
+
+
+// stdlib
+var path    = require('path');
+var assert  = require('assert');
+
+
+// internal
+var Puncher = require('..');
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+describe('Puncher', function () {
+  beforeEach(function () {
+  });
+
+
+  it("should work", function () {
+  });
+});
